9765eda39d1f9eb9ba90fb8d084faa280328c7e3
Kunal Parihar <kunal.parihar2494@gmail.com>
2026-03-30 18:50:17 +0530

    deleted unwanted files

--- src/components/Orders/EmptyState.tsx
+++
@@ -1,9 +0,0 @@
-import { Text, View } from "react-native";
-
-export default function EmptyState({ text }: { text: string }) {
-    return (
-        <View className="flex-1 items-center justify-center">
-            <Text className="text-gray-400 text-sm">{text}</Text>
-        </View>
-    );
-}
